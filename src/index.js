d9fde4841fc9042c23b3e5feb0ce10c71fe1f826
Rahul BK <rlbishwokarma@gmail.com>
2024-07-16 15:53:02 +0545

    connect to mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import envConfig from "./config/envConfig.js";
+import connectDB from "./config/db.js";
 
 const app = express();
 const port = envConfig.port;
@@ -7,6 +8,8 @@ const port = envConfig.port;
 app.get("/", (req, res) => {
   res.send("Hello Nodejs developer.");
 });
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server running at port: ${port}`);
