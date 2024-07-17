f40603b05c1c2ce7120a4956aa4fc7f394173fbf
Rahul BK <rlbishwokarma@gmail.com>
2024-07-17 21:27:53 +0545

    user controller and routes configured

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import envConfig from "./config/envConfig.js";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
@@ -16,5 +17,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: "true", limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes
+app.use("/api/v1/users", userRouter);
 
 export { app };
