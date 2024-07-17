d61109b6a001b780f2172ecfb88c05e661fbb4db
Rahul BK <rlbishwokarma@gmail.com>
2024-07-17 11:40:43 +0545

    user and video model with jwt added

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -6,6 +6,10 @@ const _config = {
   databaseUrl: process.env.MONGO_URI,
   frontendDomain: process.env.FRONTEND_DOMAIN,
   corsOrigin: process.env.CORS_ORIGIN,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY,
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+  refreshTokenExpiry: process.env.REFRESH_TOKEN_EXPIRY,
 };
 
 export default Object.freeze(_config);
