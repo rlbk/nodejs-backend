ad47b4bb9525b43eafcc284c86c95c6bc7d21188
Rahul BK <rlbishwokarma@gmail.com>
2024-07-18 13:49:12 +0545

    register controll and bugfix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,59 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "OK",
+  // get user details from frontend
+  const { email, username, fullName, password } = req.body;
+
+  // validation - not empty
+  if (
+    [fullName, username, fullName, password].some(
+      (field) => field?.trim() === ""
+    )
+  )
+    throw new ApiError(400, "All fields are required");
+
+  // check if user already exists : username, email
+  const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+  if (existingUser)
+    throw new ApiError(409, "User with email or username already exists.");
+
+  // check for images, check for avatar
+  const avatarLocalPath = req.files?.avatar?.[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+  if (!avatarLocalPath) throw new ApiError(400, "Avatar is required.");
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) throw new ApiError(400, "Avatar is required.");
+
+  console.log({ avatar, coverImage });
+
+  // create user object - create entry in db
+  const newUser = await User.create({
+    fullName,
+    username: username.toLowerCase(),
+    email,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    password,
   });
+
+  // remove password and refresh token field from response
+  const createUser = await User.findById(newUser._id).select(
+    "-password -refreshToken"
+  );
+
+  // check for user creation
+  if (createUser)
+    throw new ApiError(500, "Something went wrong while registering user");
+
+  // return res
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createUser, "User registered successfully."));
 });
 
 export { registerUser };
--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -5,8 +5,11 @@ const storage = multer.diskStorage({
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.originalname + "_" + uniqueSuffix);
+    const uniqueSuffix = Date.now();
+    const fileName = file.originalname.split(".");
+    const firstName = fileName[0].split(" ").join("_");
+    const fileSuffix = fileName[fileName.length - 1];
+    cb(null, firstName + "_" + uniqueSuffix + "." + fileSuffix);
   },
 });
 
